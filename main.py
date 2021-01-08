f37e6a4d806a857fa7aa0a157c6cf59d56bdf111
Viktor Sheverdin <viktor.sheverdin@houle.ca>
2021-01-07 17:42:54 -0800

    Finished snake without collisions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -20,6 +23,10 @@ while game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) <= 15:
+        print("Om-nom-nom")
+        food.refresh()
 
 
 screen.exitonclick()
